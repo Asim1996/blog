7981130385a4eb733a26299b09e115cb5e566720
Asim1996 <awesomeasim2596@gmail.com>
2020-04-16 20:41:31 +0530

    remove cached repo

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet')
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+
+const app = express();
+app.use(helmet())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/ping',(req,res)=>{
+return res.send("pong")
+})
+
+module.exports = app;
