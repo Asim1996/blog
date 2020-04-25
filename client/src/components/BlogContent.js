436869dd01fd1a26360cf622b8ec677a7fde7284
Asim Ansari <asim@Asims-MacBook-Air.local>
2020-04-25 22:50:17 +0530

    add html parse

--- client/src/components/BlogContent.js
+++ client/src/components/BlogContent.js
@@ -1,20 +1,44 @@
 import React from 'react'
- 
-const BlogContent = (props) => {
-    console.log(props);
-    const blog = props.location.state.blog_data;
-return (
-    <div>
-        <h1>{blog.title}</h1>
-        <section>
-            {blog.body}
-        </section>
-        <p>
-            Asim Ansari
-        </p>
-    </div> 
-   )
+import ReactHtmlParser from 'react-html-parser';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+// const BlogContent = (props) => {
+//     ReactHtmlParser
+//     console.log(props);
+//     const blog = props.location.state.blog_data;
+//     return (
+//         <div>
+//             <h1>{blog.title}</h1>
+//             <section>
+//                 ReactHtmlParser({blog.body})
+//         </section>
+//             <p>
+//                 Asim Ansari
+//         </p>
+//         </div>
+//     )
 
+// }
+
+class BlogContent extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        //   const html = '<div>Example HTML string</div>';
+        //   return <div>{ ReactHtmlParser(html) }</div>;
+        const blog = this.props.location.state.blog_data;
+        return (
+            <Container maxWidth="sm">
+                <Typography variant="h1" component="h2">
+                    {blog.title}
+                </Typography>
+                <section>
+                { ReactHtmlParser(blog.body) }
+                </section>
+
+            </Container>
+        )
+    }
 }
-
 export default BlogContent;
